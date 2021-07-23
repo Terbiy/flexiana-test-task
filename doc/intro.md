e437fcbc768c378f3c243b9c845443f48a4d866a
German Tebiev <terbiyarchitect@gmail.com>
2021-07-23 16:03:12 +0300

    Apply lein app template to create basis for Flexiana test task

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to flexiana-test-task
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/flexiana_test_task/core_test.clj
@@ -0,0 +1,7 @@
+(ns flexiana-test-task.core-test
+  (:require [clojure.test :refer :all]
+            [flexiana-test-task.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
