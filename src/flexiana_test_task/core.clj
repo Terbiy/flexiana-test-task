6e95c419981ebb8e1d50e0d74cb5c9aa44332279
German Tebiev <terbiyarchitect@gmail.com>
2021-07-25 16:30:23 +0300

    Implement solution for Task 1

--- src/flexiana_test_task/core.clj
+++ src/flexiana_test_task/core.clj
@@ -1,7 +1,28 @@
 (ns flexiana-test-task.core
+  (:require [clojure.spec.alpha :as spec])
   (:gen-class))
 
+(defn- enough-of-character-in-pool?
+  [characters-pool [character demand]]
+  (let [supply (get characters-pool character 0)] (<= demand supply)))
+
+(defn- enough-characters-in-pool?
+  [characters-pool characters-demand]
+  (->> (map (partial enough-of-character-in-pool? characters-pool)
+         characters-demand)
+       (every? true?)))
+
+(spec/def ::scramblable-string
+  #(boolean (and (string? %) (re-matches #"[a-z]*" %))))
+
+(defn scramble?
+  [source-string substring-candidate]
+  {:pre [(spec/valid? ::scramblable-string source-string)
+         (spec/valid? ::scramblable-string substring-candidate)]}
+  (enough-characters-in-pool? (frequencies source-string)
+                              (frequencies substring-candidate)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "For creation during the task #2."
   [& args]
-  (println "Hello, World!"))
+  (println "Not yet implemented."))
--- test/flexiana_test_task/core_test.clj
+++ test/flexiana_test_task/core_test.clj
@@ -2,6 +2,56 @@
   (:require [clojure.test :refer :all]
             [flexiana-test-task.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest expected-cases
+  (testing "Successful inclusion of substring"
+           (is (true? (scramble? "rekqodlw" "world")))
+           (is (true? (scramble? "cedewaraaossoqqyt" "codewars"))))
+
+  (testing "Failing inclusion of substring"
+           (is (false? (scramble? "katas" "steak")))
+           (is (false? (scramble? "aaaccc" "bbbddd"))))
+
+  (testing "Empty strings"
+           (is (true? (scramble? "hello" ""))
+               "An empty string is treated as composable from anything")
+
+           (is (true? (scramble? "" ""))
+               "Empty string even could be composed of another empty string")
+
+           (is (false? (scramble? "" "anything"))
+               "Empty string can't compose anything meaningful")))
+
+(deftest incorrect-input
+  (testing
+    "Strings containing something other than letters from a to z are not allowed"
+    (is (thrown? AssertionError (scramble? "ASDASD" "ASD") "")
+        "Even capital letters are not supported as per the task description")
+
+    (is (thrown? AssertionError (scramble? "32312" "999"))
+        "Numbers are not supported")
+
+    (is
+      (thrown? AssertionError (scramble? "👨‍👩‍👦" "👨"))
+      "This strictness saves us from handling obscure UTF composed characters")
+
+    (is (thrown? AssertionError (scramble? "asd123" "asd"))
+        "An incorrect source string leads to exception")
+
+    (is (thrown? AssertionError (scramble? "asd" "asd123"))
+        "An incorrect substring candidate leads to exception"))
+
+  (testing
+    "Any type of non-string parameter leads to an exception."
+    (let [DIFFERENT_NON_STRING_VALUES [nil 1 1/2 1.0 36786883868216818816N
+                                       3.14159265358M \a :a '() [] {} #{}]
+          PAIRS_OF_DIFFERENT_NON_STRING_VALUES (for
+                                                 [a DIFFERENT_NON_STRING_VALUES
+                                                  b DIFFERENT_NON_STRING_VALUES]
+                                                 [a b])
+          throws? (fn [function arguments]
+                    (try (do (apply function arguments) false)
+                         (catch AssertionError _ true)))]
+
+      (is (true? (->> (map #(throws? scramble? %)
+                        PAIRS_OF_DIFFERENT_NON_STRING_VALUES)
+                      (every? true?)))))))
